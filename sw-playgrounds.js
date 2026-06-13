3a1859eac814b1b17d12792eb33b9d830d3fa55b
sgbtw <sgbtwrgmn554@gmail.com>
2026-06-13 04:25:49 +0300

    Remove user/PRO button from header

--- sw-playgrounds.js
+++ sw-playgrounds.js
@@ -1,4 +1,4 @@
-const CACHE_STATIC = 'playgrounds-static-v18';
+const CACHE_STATIC = 'playgrounds-static-v19';
 const CACHE_TILES  = 'playgrounds-tiles-v3';
 const CACHE_DATA   = 'playgrounds-data-v2';
 
